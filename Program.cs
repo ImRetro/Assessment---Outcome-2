6b0bbf86340bc2d540a6b5e478c442572f370812
ImRetro <all.of.us.lewis@live.co.uk>
2017-01-24 15:57:12 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,10 +11,12 @@ namespace Outcome_2
 
         // Global Variables
         public static string[] Array1;
-        public static string[] firstname;
-        public static string[] secondname;
-        public static int[] score;
-        public static string winner;
+        public static string firstname = "";
+        public static string secondname = "";
+        public static int score;
+        public static string winner = "";
+
+
 
 
         //Execute other functions
@@ -38,30 +40,25 @@ namespace Outcome_2
             // identifys the location of the file , and reads in the file
             try
             {
+                // reads in from file
                 StreamReader reader = new StreamReader(@"\\GCASTUDENTFS.glasgowclyde.ac.uk\homedrives$\20178947\Work\Outcome 2 Assesment\Data.csv");
                 Data = reader.ReadToEnd();
                 Console.WriteLine("Found File");
 
             }
+                //activates if the file path is incorrect or cannot happen
             catch (FileNotFoundException)
             {
                 Console.WriteLine("File has not been found. Please try again.");
                 Console.Read();
             }
 
-
+            //separates the array
             Char [] Punctuation = new char[] { '\n', ',' };
             Array1 = Data.Split(Punctuation);
 
-            for (int i = 2; i < Array1.Length; i+=3)
-            {
-                Console.WriteLine(Array1[i]);
-                score = int.Parse(Console.ReadLine());
-
-
-            }
-
-
+            
+            //returns data
                 return Data;
 
 
@@ -69,37 +66,64 @@ namespace Outcome_2
         public static void findhighmark()
         {
 
+            //sets score as a element from the array
+            score = int.Parse(Array1[2]);
 
+            // local variables
+            int Mark;
 
+            //starts a loop
+            for (int i = 2; i < Array1.Length; i+= 3)
 
-
-
-            winner = Array1[2];
-
-
-            for (int i = 2; i < Array1.Length; i +=3)
             {
 
-                Console.WriteLine(Array1[i]);
-             
-
-               
-                {
-
-                }
+                //sets mark to the current element in the array
+                Mark = int.Parse(Array1[i]);
                 
+
+                //compares mark with score
+                if (Mark > score)
+                {
+                    //sets information
+                    score = Mark;
+                    firstname = Array1[i - 2];
+                    secondname = Array1[i - 1];
                     
+                    
+                }
             }
-           
-            Console.Read();
+
+            //puts data into 1 variable
+            winner = (firstname + " " + secondname + " "  + score);
+            Console.WriteLine(winner);
+            
 
 
         }
         public  static void displayresults()
         {
-            StreamWriter writer = new StreamWriter(@"U:\Work\Outcome 2 Assesment\Results.txt");
-            writer.WriteLine(winner);
+            //trys to do the action
+            try
+            {
 
+                //writes to a file
+                StreamWriter writer = new StreamWriter(@"\\GCASTUDENTFS.glasgowclyde.ac.uk\homedrives$\20178947\Work\Outcome 2 Assesment\Results.txt");
+                writer.Flush();
+                writer.WriteLine(winner);
+                writer.Close(); 
+                Console.WriteLine("Written to file");
+                Console.Read();
+
+            }
+                //activates if the file path is incorrect or cannot happen
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File Not Found");
+                Console.Read();
+
+            }
+
+          
 
 
 
